83521514bf547ad15120e57c00c0e30950444c81
juliekelso <julia.kelso@gmail.com>
2016-01-25 00:50:59 -0700

    going to combine all nutirent plus indices output

--- CorrsSFS2016.R
+++ CorrsSFS2016.R
@@ -1,18 +1,18 @@
 library(tidyr)
+library(stats)
+library(reshape2)
 
 
+getwd()
+[1] "C:/Users/Julie Kelso/Dropbox/SFS/SFS 2016/SFSAbstract2016"
 
-olddata_wide
-#>   subject sex control cond1 cond2
-#> 1       1   M     7.9  12.3  10.7
-#> 2       2   F     6.3  10.6  11.1
-#> 3       3   F     9.5  13.1  13.8
-#> 4       4   M    11.5  13.4  12.9
+All = read.csv("C:/Users/Julie Kelso/Dropbox/SFS/SFS 2016/SFSAbstract2016/All.csv")
+Ind = read.csv("C:/Users/Julie Kelso/Dropbox/SFS/SFS 2016/SFSAbstract2016/IndexWsites.csv")
+colnames(All)
 
-# The arguments to gather():
-# - data: Data object
-# - key: Name of new key column (made from names of data columns)
-# - value: Name of new value column
-# - ...: Names of source columns that contain values
-data_long <- gather(olddata_wide, condition, measurement, control:cond2)
-data_long
+aql <- melt(airquality, id.vars = c("month", "day"))
+aqw <- dcast(aql, month + day ~ variable)
+
+w = dcast(All, Day + Month + Year + SiteCode + Watershed  ~ VariableCode, value.var = "DataValue", fun.aggregate = mean)
+
+write.csv(w, "Allwide.csv")
