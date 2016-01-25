7669aa7e5a5054590c95a646c3f57f8aa7e383c4
juliekelso <julia.kelso@gmail.com>
2016-01-25 01:07:17 -0700

    JIC

--- CorrsSFS2016.R
+++ CorrsSFS2016.R
@@ -16,3 +16,10 @@ aqw <- dcast(aql, month + day ~ variable)
 w = dcast(All, Day + Month + Year + SiteCode + Watershed  ~ VariableCode, value.var = "DataValue", fun.aggregate = mean)
 
 write.csv(w, "Allwide.csv")
+
+colnames(Ind)[c(6)] <- c("SiteCode")
+
+m = merge(w, Ind, by=c("Month","Year","SiteCode"), all.y = TRUE)
+
+######3 remove duplicates according to multiple columns #####
+done = m[!duplicated(m[c("Sample")]),]
